ba020640ec651f30db066c2ea9015d26cd8a6c03
Kristian Arnesen Vik <kristianarnesenvik@gmail.com>
2021-11-08 15:04:07 +0100

    added image modal functionality

--- js/webflow.js
+++ js/webflow.js
@@ -485,6 +485,48 @@ Webflow.require = function (name) {
   return modules[name];
 };
 
+// MODAL CODE
+
+// create references to the modal...
+console.log("hei!");
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    console.log(evt);
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(this);
+  }
+}
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) {
+      isEscape = (evt.key === "Escape" || evt.key === "Esc");
+  } else {
+      isEscape = (evt.keyCode === 27);
+  }
+  if (isEscape) {
+      modal.style.display = "none";
+  }
+};
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
 function bindModule(module) {
   // If running in Webflow app, subscribe to design/preview events
   if (Webflow.env()) {
